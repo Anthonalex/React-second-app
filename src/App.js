73724701b426c94fc50494f63a91cb3eea2c7604
Anthonalex <Anthonalex@users.noreply.github.com>
2021-04-23 22:50:59 +0400

    simple calc done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import Game from "./components/tic-tac-toe/tic-tak-toe";
+import Calc from "./components/simpleCalculator/simpleCalc";
 
 function App() {
   return (
     <div>
       <h1>tic tac toe</h1>
-      <Game></Game>
+      <Game />
+      <br />
+      <br />
+      <br />
+      <Calc />
     </div>
   );
 }
---
+++ src/components/simpleCalculator/simpleCalc.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "./simpleCalc.css";
+
+export default class Calc extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstInputValue: 0,
+      secondInputValue: 0,
+      value: 0,
+    };
+  }
+
+  handleFirstChange = (e) => {
+    this.setState({
+      firstInputValue: Number(e.target.value),
+    });
+  };
+
+  handleSecondChange = (e) => {
+    this.setState({
+      secondInputValue: Number(e.target.value),
+    });
+  };
+
+  handleInc = () => {
+    const res = this.state.firstInputValue + this.state.secondInputValue;
+    this.setState({
+      value: res,
+    });
+  };
+
+  handleDec = () => {
+    const res = this.state.firstInputValue - this.state.secondInputValue;
+    this.setState({
+      value: res,
+    });
+  };
+
+  handleMul = () => {
+    const res = this.state.firstInputValue * this.state.secondInputValue;
+    this.setState({
+      value: res,
+    });
+  };
+
+  handleDiv = () => {
+    const res = this.state.firstInputValue / this.state.secondInputValue;
+    this.setState({
+      value: res,
+    });
+  };
+
+  render() {
+    return (
+      <div className="calc-div">
+        <h1>Simple Calc</h1>
+        <br />
+        <input
+          type="number"
+          className="calc-input"
+          onChange={this.handleFirstChange}
+        />
+        <button onClick={this.handleInc}>+</button>
+        <button onClick={this.handleDec}>-</button>
+        <button onClick={this.handleMul}>*</button>
+        <button onClick={this.handleDiv}>/</button>
+        <input
+          type="number"
+          className="calc-input"
+          onChange={this.handleSecondChange}
+        />
+        =
+        <input
+          type="number"
+          disabled="true"
+          className="calc-input"
+          value={this.state.value}
+        />
+      </div>
+    );
+  }
+}
