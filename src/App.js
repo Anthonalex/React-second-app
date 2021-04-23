f5b71d88052a74fade5c705a08e3661bbd32ab60
Anthonalex <Anthonalex@users.noreply.github.com>
2021-04-23 19:23:00 +0400

    timer added to the game, 6 seconds each player

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Game from "./components/tic-tac-toe/tic-tak-toe";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>tic tac toe</h1>
+      <Game></Game>
     </div>
   );
 }
---
+++ src/components/tic-tac-toe/tic-tak-toe.jsx
@@ -0,0 +1,150 @@
+import "./style.css";
+
+import React from "react";
+import { render } from "react-dom";
+import { Board } from "./board.jsx";
+
+export default class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startBtn: false,
+      time: 6,
+      history: [
+        {
+          squares: Array(9).fill(null),
+        },
+      ],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+  }
+
+  handleStart = () => {
+    this.setState({
+      startBtn: true,
+    });
+    let id = setInterval(() => {
+      this.setState((prevState) => ({
+        time: prevState.time - 1,
+      }));
+
+      if (this.state.time === 0) {
+        clearInterval(id);
+        alert(`${this.state.xIsNext ? "O" : "X"} Wins!`);
+        this.setState({
+          startBtn: false,
+          time: 6,
+          stepNumber: 0,
+          history: [
+            {
+              squares: Array(9).fill(null),
+            },
+          ],
+        });
+      }
+    }, 1000);
+  };
+
+  handleClick(i) {
+    if (!this.state.startBtn) {
+      this.handleStart();
+    }
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares,
+        },
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+      time: 6,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+          <div>
+            <p>{this.state.time} Seconds</p>
+            <button onClick={this.handleStart} disabled={this.state.startBtn}>
+              START
+            </button>
+          </div>
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(
+  <div>
+    <Game />
+  </div>,
+  document.getElementById("root")
+);
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
